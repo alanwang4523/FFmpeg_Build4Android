050d263be5bd515ec9caf83e5d105248d938a35a
alanwang4523 <alanwang4523@gmail.com>
2020-11-05 11:30:52 +0800

    增加命令行参数解析

--- FFmpegJNI4Android/app/src/main/cpp/native-lib.cpp
+++ FFmpegJNI4Android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,64 @@
 #include <jni.h>
 #include <string>
+#include <malloc.h>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_alan_ffmpegjni4android_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#include <android/log.h>
+
+#define LOGE(TAG, ...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+
+int ffmpeg_run(int argc, char **argv) {
+
+    LOGE("ffmpeg_run", "argc = %d", argc);
+    for (int i = 0; i < argc; ++i) {
+        LOGE("ffmpeg_run", "argv[%d] = %s", i, argv[i]);
+    }
+    return 0;
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_alan_ffmpegjni4android_MainActivity_exeFFmpegCommand(
+        JNIEnv* env, jobject obj, jstring js_cmd) {
+//    std::string hello = "Hello from C++";
+//    return env->NewStringUTF(hello.c_str());
+
+    jboolean is_copy = 0;
+    const char* c_command = (char*)env->GetStringUTFChars(js_cmd, &is_copy);
+    int c_command_len = strlen(c_command);
+
+    int argc = 0;
+    char* argv[200];// 命令参数个数 200
+    char temp_arg[500]; // 每个参数最长字符数 500
+    int char_count = 0;
+
+    for(int i = 0; i <= c_command_len; i++){
+        //将当前空格或终止符与前一个空格之间的字符串存放到 argv[argc] 中
+        if(c_command[i] == ' ' || c_command[i] == '\0'){
+            // 为当前参数分配内存，长度为：字符个数 char_count + 终止符
+            argv[argc] = (char*)calloc(char_count + 1, sizeof(char));
+            memcpy(argv[argc], temp_arg, char_count);
+            strcat(argv[argc], "\0");
+
+            argc++;
+            char_count = 0;
+            i++;
+        }
+
+        temp_arg[char_count] = c_command[i];
+        char_count++;
+    }
+
+    // 执行 ffmpeg 命令
+    int ret = ffmpeg_run(argc, argv);
+
+    // 释放内存
+    for(int i = 0; i < argc; i++){
+        free(argv[i]);
+        argv[i] = NULL;
+    }
+
+    if(c_command != NULL) {
+        env->ReleaseStringUTFChars(js_cmd, c_command);
+    }
+
+    return ret;
 }
--- FFmpegJNI4Android/app/src/main/java/com/alan/ffmpegjni4android/MainActivity.java
+++ FFmpegJNI4Android/app/src/main/java/com/alan/ffmpegjni4android/MainActivity.java
@@ -3,6 +3,7 @@ package com.alan.ffmpegjni4android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,54 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+//    private static final String TEST_CMD_STR = " -i stmedia:/sdcard/Alan/ffmpeg/test.mp4";
+
+    // TODO 多视频合并 都 OK
+//     -y -f concat -safe 0 -i /sdcard/Alan/ffmpeg/src/input_files.txt -c copy /sdcard/Alan/ffmpeg/output_concat.mp4
+    private static final String TEST_CMD_STR = " -y -f concat -safe 0 -i /sdcard/Alan/ffmpeg/src/input_files.txt " +
+            "-c copy /sdcard/Alan/ffmpeg/output_concat.mp4";
+
+    // TODO 音频 + 图片 生成视频用于分享 OK
+    // -y -loop 1 -r 0.1 -i /sdcard/Alan/ffmpeg/src/image_01.png -i /sdcard/Alan/ffmpeg/src/music.m4a -c:a copy -c:v libopenh264 -allow_skip_frames 1 -g 30 -r 15 -t 9000 -shortest /sdcard/Alan/ffmpeg/av_video.mp4
+//    private static final String TEST_CMD_STR = " -y -loop 1 -r 0.1 -i /sdcard/Alan/ffmpeg/src/image_01.png -i /sdcard/Alan/ffmpeg/src/music.m4a " +
+//            "-c:a copy -c:v libopenh264 -allow_skip_frames 1 -g 30 -r 15 -t 9000 -shortest /sdcard/Alan/ffmpeg/av_video.mp4";
+
+    // TODO 音频 + 视频 Muxer OK
+    // -y -i /sdcard/Alan/ffmpeg/src/st_test_01.mp4 -i /sdcard/Alan/ffmpeg/src/music.m4a -c copy /sdcard/Alan/ffmpeg/muxer_out.mp4
+//    private static final String TEST_CMD_STR = String.format(" -y -i %s -i %s -c copy %s",
+//            "/sdcard/Alan/ffmpeg/src/st_test_01.mp4", "/sdcard/Alan/ffmpeg/src/music.m4a", "/sdcard/Alan/ffmpeg/muxer_out.mp4");
+
+    // TODO 音视频旋转 OK
+    // -y -i /sdcard/Alan/ffmpeg/src/st_test_01.mp4 -c copy -metadata:s:v rotate=180 /sdcard/Alan/ffmpeg/out_rotate.mp4
+//    private static final String TEST_CMD_STR = String.format(" -y -i %s -c copy -metadata:s:v rotate=%d %s",
+//            "/sdcard/Alan/ffmpeg/src/st_test_01.mp4", 180, "/sdcard/Alan/ffmpeg/out_rotate.mp4");
+
+    // TODO 音视频压缩 OK
+    // -y -i /sdcard/Alan/ffmpeg/4.mp4 -threads 4 -b:v 1000k -vcodec libopenh264 -async 1 -vsync 1 -f mp4 -movflags faststart -vf scale=480:-1 -strict -2 -an /sdcard/Alan/ffmpeg/4_video.mp4
+//    private static final String TEST_CMD_STR = String.format(Locale.US, " -y -i %s -threads 4 -b:v %dk -vcodec libopenh264 " +
+//                    "-async 1 -vsync 1 -f mp4 -movflags faststart -vf scale=%d:-1 -strict -2 -an %s",
+//            "/sdcard/Alan/ffmpeg/4.mp4", 1000, 480, "/sdcard/Alan/ffmpeg/4_video.mp4");
+
+    // TODO 音视频分离 OK
+    // -y -i /sdcard/Alan/ffmpeg/4.mp4 -threads 4 -vcodec copy -f mp4 -movflags faststart -strict -2 -an /sdcard/Alan/ffmpeg/4_video.mp4 -acodec aac -vn /sdcard/Alan/ffmpeg/4_audio.aac
+//    private static final String TEST_CMD_STR = String.format(Locale.US, " -y -i %s -threads 4 " +
+//                    "-vcodec copy -f mp4 -movflags faststart -strict -2 -an %s -acodec aac -vn %s",
+//        "/sdcard/Alan/ffmpeg/4.mp4", "/sdcard/Alan/ffmpeg/4_video.mp4", "/sdcard/Alan/ffmpeg/4_audio.aac");
+
+    // TODO 音视频裁剪 OK
+    // -y -i /sdcard/Alan/ffmpeg/4.mp4 -threads 4 -b:v 200k -vcodec copy -acodec copy -ss 00:00:10 -t 00:00:30 -f mp4 -movflags faststart -strict -2 -an /sdcard/Alan/ffmpeg/out_trim.mp4
+//    private static final String TEST_CMD_STR = String.format(Locale.US, " -y -i %s -threads 4 -b:v %dk " +
+//                    "-vcodec copy -acodec copy -ss 00:00:10 -t 00:00:30 -f mp4 -movflags faststart -strict -2 -an %s",
+//            "/sdcard/Alan/ffmpeg/4.mp4", 200, "/sdcard/Alan/ffmpeg/out_trim.mp4");
+
+    // TODO 加水印 Ok
+    // -y -i /sdcard/Alan/ffmpeg/test.mp4 -i /sdcard/Alan/ffmpeg/video_watermark.png -b:v 5120k -acodec copy -filter_complex "[1]scale=200:-1[water];[0][water]overlay=main_w-overlay_w-20:main_h-overlay_h-20" -filter_complex_threads 4  -f mp4 -vcodec libopenh264 /sdcard/Alan/ffmpeg/out_watermark.mp4
+//    private static final String TEST_CMD_STR = String.format(Locale.US, " -y -i %s -i %s -b:v %dk -acodec copy " +
+//                    "-filter_complex \"[1]scale=%d:-1[water];[0][water]overlay=main_w-overlay_w-%d:main_h-overlay_h-%d\" " +
+//                    "-filter_complex_threads 4  -f mp4 -vcodec libopenh264 %s",
+//        "/sdcard/Alan/ffmpeg/test.mp4", "/sdcard/Alan/ffmpeg/video_watermark.png", 5 * 1024, 200, 20, 20, "/sdcard/Alan/ffmpeg/out_watermark.mp4");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +68,18 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        tv.setText(stringFromJNI());
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exeFFmpegCommand(TEST_CMD_STR.trim());
+            }
+        });
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native int exeFFmpegCommand(String cmdStr);
 }
